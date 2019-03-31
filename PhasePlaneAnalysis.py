df73c8ff7df83bcc3cd58aaebe80bcb317f1d809
GSUAD\rkrylov1 <rkrylov1@student.gsu.edu>
2019-03-31 18:22:43 -0400

    added method to generate phase plane trajectories without drawing errors. Working on cleaning up the methods and make them easier to use.

--- PhasePlaneAnalysis.py
+++ PhasePlaneAnalysis.py
@@ -53,8 +53,8 @@ def many_trajectories(w, h, a):
     plt.figure(figsize=(12,8), dpi=80)
     plt.xlim(-10, 10)
     plt.ylim(-4,4)
-    plt.xlabel('phi')
-    plt.ylabel('phi_dot')
+    plt.xlabel(r'$\phi$')
+    plt.ylabel(r'$\dot{phi}$')
     print(phi_integrated_arr)
     
     row, columns = sol_arr.shape
@@ -72,7 +72,7 @@ def diff_normalization(difference):
     size = difference.size
     diff_2d = difference.reshape((int(size/2), 2))
     row_norms = np.array([np.linalg.norm(row) for row in diff_2d])
-    diff_normalized = diff_2d/(18*row_norms[:, None])
+    diff_normalized = diff_2d/(row_norms[:, None])
     
     return diff_normalized
 
@@ -101,15 +101,17 @@ def selected_traj(func, params, coords):
     
 def time_series(func, params, fig_name):
     p = params[-1]
-    t = np.linspace(0, 100, 10000)
-    phi_x = [-0.5, 2, 0.1, 0.1]
+    t = np.linspace(0, 5000, 100000)
+    phi_x = [0.1, 0.1, 0.75, 0]
+    #phi_x = [-0.5, 2, 0.1, 0.1]
     sol = odeint(func, phi_x, t, args = params)
-    x = sol[:, 2]
-    """
-    phi = sol[:, 0]
     
-    neg_phi = np.where(phi<-1)
-    pos_phi = np.where(phi>1)
+    x = sol[99500:, 2]
+    """
+    #phi = sol[99000:, 0]
+    
+    neg_phi = np.where(phi<-p)
+    pos_phi = np.where(phi>p)
     phi[neg_phi] = np.mod(phi[neg_phi], p)
     phi[pos_phi] = np.mod(phi[pos_phi], -p)
     """
@@ -120,7 +122,7 @@ def time_series(func, params, fig_name):
     plt.ylabel(r"$x$")
     
     print(sol[:, 0])
-    plt.plot(t, x)
+    plt.plot(t[99500:], x)
     plt.savefig("./{}.pdf".format(fig_name))
     plt.show()
     
@@ -136,26 +138,26 @@ def vector_field(bounds, coords, fig_name, func, params):
     t = np.linspace(0, 1, 10)
     phi_integrated_arr = np.empty((10, 2))
     for phi in xy[1:]:
-        solution  = odeint(func, phi, t, args=params)[:, 2:]
+        solution  = odeint(func, phi, t, args=params)[:, :2]
         phi_integrated_arr = concat((phi_integrated_arr, solution), axis=1)
         
-    sol_arr = phi_integrated_arr[:, 2:]
+    sol_arr = phi_integrated_arr[:, :2]
     difference = sol_arr - np.roll(sol_arr, 1, axis=0)
     sol_arr = sol_arr[0]
     difference = difference[1]
-    diff_normalized = diff_normalization(difference)
+    diff_normalized = diff_normalization(difference)/18
     
     
     
     plt.rcParams.update({'font.size':20})
     plt.figure(figsize=(12,8), dpi=80)
     #plt.xlim(-pi/2, pi/2)
-    plt.xlim(-p, p)
-    plt.ylim(-3,3)
-    plt.xlabel(r'$x$', labelpad=200, fontweight=1000)
-    plt.ylabel(r'$\dot{x}$', labelpad = 310, fontweight = 1000)
+    plt.xlim(bounds[0][0], bounds[0][0] + bounds[1])
+    plt.ylim(bounds[0][1], bounds[0][1] + bounds[1])
+    plt.xlabel(r'$\phi$', labelpad=200, fontweight=1000)
+    plt.ylabel(r'$\dot{\phi}$', labelpad = 310, fontweight = 1000)
     plt.title("Phase Diagram for Pedestrian-Bridge System")
-    plt.suptitle(r"w = 1, $\lambda = 0.5$, a = 0.2", y = 0.965)
+    plt.suptitle(r"w = {}, $\lambda = {}$, a = {}".format(params[1], params[4], params[5]), y = 0.965)
     #moving axis
     # Move left y-axis and bottim x-axis to centre, passing through (0,0)
     ax = plt.subplot()
@@ -186,16 +188,23 @@ def vector_field(bounds, coords, fig_name, func, params):
     for start, change in zip(sol, diff_normalized):
         plt.arrow(start[0], start[1], change[0], change[1], width = 0.01, color="#808080")
         
-    t2 = np.linspace(0, 100, 10000)
+    """
+    #\dot{x} vs x                       
+    t2 = np.linspace(0, 5000, 100000)
+    #phi_x = [-0.5, 2, 0.1, 0.1]
     phi_x = [0.1, 0.1, 0.75, 0]
+    #phi_x2 = [0.1, 0.1, -0.02, 0]
     traj = odeint(func, phi_x, t2, args=params)
     
-    plt.plot(traj[:,2], traj[:, 3], color='b')
+    #traj2 = odeint(func, phi_x2, t2, args=params)
+    
+    plt.plot(traj[90000:,2], traj[90000:, 3], color='b')
+    #plt.plot(traj2[:, 2], traj2[:, 3])
     
     """
     #\phi, \dot{\phi} trajectory
-    t2 = np.linspace(0, 10, 10000)
-    phi_x = [-0.5, -2, 0, 0]
+    t2 = np.linspace(0, 5000, 100000)
+    phi_x = [0.1, 0.1, 0.75, 0]
     traj = odeint(func, phi_x, t2, args = params)    
     
     phi = traj[:, 0]
@@ -204,8 +213,8 @@ def vector_field(bounds, coords, fig_name, func, params):
     phi[neg_phi] = np.mod(phi[neg_phi], p)
     phi[pos_phi] = np.mod(phi[pos_phi], -p)
     
-    plt.plot(phi, traj[:, 1], color='b')                           
-    """
+    plt.plot(phi[99000:], traj[99000:, 1], color='b')                           
+
     #for traj in selected_traj(func, params, coords):
     #    plt.plot(traj[:,0], traj[:, 1], color='b')
         
@@ -221,17 +230,91 @@ def vector_field(bounds, coords, fig_name, func, params):
     plt.savefig("./{}.pdf".format(fig_name))
     plt.show()
     
-bounds = ((-3, -3), 6, 60)
+    
+def phase_plane_traj(func, coords, params, bounds, fig_name):
+    p = params[-1]
+
+    plt.rcParams.update({'font.size':20})
+    plt.figure(figsize=(12,8), dpi=80)
+    #plt.xlim(-pi/2, pi/2)
+    plt.xlim(bounds[0][0], bounds[0][0] + bounds[1])
+    plt.ylim(bounds[0][1], bounds[0][1] + bounds[1])
+    plt.xlabel(r'$x$', labelpad=200, fontweight=1000)
+    plt.ylabel(r'$\dot{x}$', labelpad = 310, fontweight = 1000)
+    plt.title("Phase Diagram for Pedestrian-Bridge System")
+    plt.suptitle(r"w = {}, $\lambda = {}$, a = {}".format(params[1], params[4], params[5]), y = 0.965)
+    #moving axis
+    # Move left y-axis and bottim x-axis to centre, passing through (0,0)
+    ax = plt.subplot()
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+    ax.xaxis.get_ticklabels
+    
+    # Eliminate upper and right axes
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    
+    ax.axhline(linewidth=4, color='k')
+    ax.axvline(linewidth=4, color='k')
+    
+    xticks = ax.xaxis.get_major_ticks()
+    xticks[4].label1.set_visible(False)
+    
+    #yticks = ax.yaxis.get_major_ticks()
+    #yticks[3].update_position(200)
+    
+    
+    plt.xticks(fontweight=700)
+    plt.yticks(fontweight=700)
+    
+    
+    #\dot{x} vs x                       
+    t2 = np.linspace(0, 5000, 100000)
+    #phi_x = [-0.5, 2, 0.1, 0.1]
+    phi_x = [0.1, 0.1, 0.75, 0]
+    #phi_x2 = [0.1, 0.1, -0.02, 0]
+    traj = odeint(func, phi_x, t2, args=params)
+    
+    #traj2 = odeint(func, phi_x2, t2, args=params)
+    
+    plt.plot(traj[90000:,2], traj[90000:, 3], color='b')
+    #plt.plot(traj2[:, 2], traj2[:, 3])
+    
+    """
+    #\phi, \dot{\phi} trajectory
+    t2 = np.linspace(0, 5000, 100000)
+    phi_x = [0.1, 0.1, 0.75, 0]
+    traj = odeint(func, phi_x, t2, args = params)    
+    
+    phi = traj[:, 0]
+    neg_phi = np.where(phi<-p)
+    pos_phi = np.where(phi>p)
+    phi[neg_phi] = np.mod(phi[neg_phi], p)
+    phi[pos_phi] = np.mod(phi[pos_phi], -p)
+    
+    plt.plot(phi[99000:], traj[99000:, 1], color='b')     
+    """
+    
+    
+    plt.savefig("./{}.pdf".format(fig_name))
+    plt.show()
+    
+    
+    return
+    
+bounds = ((-0.3, -0.3), 0.6, 60)
 #coords_sp1 = (())
 #coords_sp2 = 
 coords_bp = ()
 #params: (mu, w, g, l, lambda, a, k, h, p)
-params = (0.0001, 1, 10, 1, 0.5, 0.2, 1, 0.05, 1)
-fig_name = "PhaseDiagramPBX"
+#params = (0.1, 0.7, 10, 1, 0.1, 0.2, 1, 0.1, 0.3)
+params = (0.1, 0.7, 10, 1, 1, 1, 0.72, 0.05, 1.5)
+fig_name = "PhaseDiagramPBPHI_mu_0.1_w_07_lambda_1_a_1_k_072_h_005_p_15"
 
-vector_field(bounds, coords_bp, fig_name, pedestrian_bridge, params)
+#vector_field(bounds, coords_bp, fig_name, pedestrian_bridge, params)
 
-time_series(pedestrian_bridge, params, "XTimeSeries1")
+phase_plane_traj(pedestrian_bridge, coords_bp, params, bounds, fig_name)
+time_series(pedestrian_bridge, params, "PHITimeSeries_mu_0.1_w_07_lambda_1_a_1_k_072_h_005_p_15")
 
 
 """
